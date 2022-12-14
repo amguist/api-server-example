e14b44b8af0316102ecf4bce62a7268897992d0d
Aaron Guist <rhon.guist@gmail.com>
2022-12-14 14:28:14 -0800

    Testing Again

--- services/hello-api-service/src/main/java/com/hawkeye/api/server/examples/hello/service/models/HelloResponse.java
+++ services/hello-api-service/src/main/java/com/hawkeye/api/server/examples/hello/service/models/HelloResponse.java
@@ -2,16 +2,27 @@ package com.hawkeye.api.server.examples.hello.service.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.Date;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HelloResponse {
 
     private String name;
+    private Date responseDate;
 
     public String getName() {
         return name;
     }
 
+    public Date getResponseDate() {
+        return responseDate;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setResponseDate(Date responseDate) {
+        this.responseDate = responseDate;
     }
 }
