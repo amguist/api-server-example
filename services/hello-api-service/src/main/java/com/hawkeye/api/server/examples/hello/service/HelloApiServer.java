30eccd6ac62c90345a949114f65f79fc31bccb31
Aaron Guist <rhon.guist@gmail.com>
2022-12-16 08:23:34 -0800

    Adding sonar plugin right now to the project

--- services/hello-api-service/src/main/java/com/hawkeye/api/server/examples/hello/service/HelloApiServer.java
+++ services/hello-api-service/src/main/java/com/hawkeye/api/server/examples/hello/service/HelloApiServer.java
@@ -7,6 +7,8 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.util.Resources;
 
+import java.awt.color.ColorSpace;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
